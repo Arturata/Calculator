d4dd9fe7b4e6a720ba53403aef0f22f74cee9d95
Arturata <104503710+Arturata@users.noreply.github.com>
2022-11-30 20:27:47 +0700

    добавлены исключения ошибок, новые функции, пояснения функций

--- Calc.py
+++ Calc.py
@@ -1,16 +1,14 @@
 import sys
 from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication, QMainWindow
-import numpy
 import math
 
 
 class Calculator(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.res = '0'
+        self.res = ''
         self.expression = ''
-
         self.lineEdit_res = None
         self.lineEdit_ex = None
         self.pushButton_eq = None
@@ -24,13 +22,13 @@ class Calculator(QMainWindow):
         self.pushButton_ch = None
         self.pushButton_deg = None
         self.pushButton_module = None
-        self.pushButton_p = None
+        self.pushButton_back = None
         self.pushButton_rd = None
         self.pushButton_sq = None
         self.pushButton_rev = None
         self.pushButton_two = None
         self.pushButton_ten = None
-        self.pushButton_mod = None
+        self.pushButton_round = None
         self.buttonGroup = None
         self.pushButton_br1 = None
         self.pushButton_br2 = None
@@ -38,9 +36,7 @@ class Calculator(QMainWindow):
         self.minning = False
         self.multing = False
         self.diving = False
-
         uic.loadUi('Calculator.ui', self)
-
         self.pushButton_eq.clicked.connect(self.eq)
         self.pushButton_cl.clicked.connect(self.clear)
         self.pushButton_plus.clicked.connect(self.plus)
@@ -52,53 +48,60 @@ class Calculator(QMainWindow):
         self.pushButton_ch.clicked.connect(self.change)
         self.pushButton_deg.clicked.connect(self.deg)
         self.pushButton_module.clicked.connect(self.module)
-        self.pushButton_p.clicked.connect(self.p)
+        self.pushButton_back.clicked.connect(self.back)
         self.pushButton_rd.clicked.connect(self.rd)
         self.pushButton_sq.clicked.connect(self.sq)
         self.pushButton_rev.clicked.connect(self.rev)
         self.pushButton_two.clicked.connect(self.two)
         self.pushButton_ten.clicked.connect(self.ten)
-        self.pushButton_mod.clicked.connect(self.mod)
+        self.pushButton_round.clicked.connect(self.round)
         [i.clicked.connect(self.brackets) for i in self.buttonGroup_br.buttons()]
         [i.clicked.connect(self.run) for i in self.buttonGroup.buttons()]
 
     def eq(self):
-        self.res = str(eval(self.expression))
-        self.lineEdit_res.setText(self.res)
-        self.lineEdit_ex.setText(self.res)
-        self.expression = self.res
-        self.res = '0'
+        """Выводит на экран значение"""
+        if self.expression != '':
+            self.res = str(eval(self.expression))
+            self.lineEdit_res.setText(self.res)
+            self.lineEdit_ex.setText(self.res)
+            self.expression = self.res
+            self.res = '0'
 
     def clear(self):
-        self.res = '0'
+        """Очищает экран"""
+        self.res = ''
         self.expression = ''
-        self.lineEdit_res.setText("0")
-        self.lineEdit_ex.setText('0')
+        self.lineEdit_res.setText('')
+        self.lineEdit_ex.setText('')
 
     def restore(self, el):
+        """Определение одной из простых операций"""
         self.plussing = el == '+'
         self.minning = el == '-'
         self.multing = el == '*'
         self.diving = el == '/'
 
     def run(self):
+        """добавление цифр"""
         self.expression += str(self.sender().text())
         self.lineEdit_ex.setText(self.expression)
-        if self.plussing and self.brackets():
-            self.lineEdit_res.setText(str(eval(self.expression)))
-        if self.minning and self.brackets():
-            self.lineEdit_res.setText(str(eval(self.expression)))
-        if self.multing and self.brackets():
-            self.lineEdit_res.setText(str(eval(self.expression)))
-        if self.diving and self.brackets():
-            self.lineEdit_res.setText(str(eval(self.expression)))
+        if self.brackets():
+            if self.plussing:
+                self.lineEdit_res.setText(str(eval(self.expression)))
+            if self.minning:
+                self.lineEdit_res.setText(str(eval(self.expression)))
+            if self.multing:
+                self.lineEdit_res.setText(str(eval(self.expression)))
+            if self.diving:
+                self.lineEdit_res.setText(str(eval(self.expression)))
 
     def brackets(self):
+        """проверка скобок"""
         if self.sender().text() == '(' or self.sender().text() == ')':
             self.expression += str(self.sender().text())
         self.lineEdit_ex.setText(self.expression)
         c = 0
-        s = "".join(filter(lambda i: i in ("(", ")"), self.expression))
+        s = "".join(filter(lambda x: x in ("(", ")"), self.expression))
         for i in s:
             if i == '(':
                 c += 1
@@ -109,69 +112,109 @@ class Calculator(QMainWindow):
         return not c
 
     def plus(self):
-        self.expression += '+'
-        self.plussing = True
-        self.restore('+')
+        """функция сложения"""
+        if self.expression[-1] != '*' and self.expression[-1] != '/':
+            self.expression += '+'
+            self.plussing = True
+            self.restore('+')
 
     def min(self):
-        self.expression += '-'
-        self.minning = True
-        self.restore('-')
+        """функция вычитания"""
+        if self.expression[-1] != '*' and self.expression[-1] != '/':
+            self.expression += '-'
+            self.minning = True
+            self.restore('-')
 
     def mult(self):
-        self.expression += '*'
-        self.multing = True
-        self.restore('*')
+        """функция умножения"""
+        if self.expression[-2:] != '//' and (self.expression[-1] != '/' and self.expression[-1] != '+' and
+                                             self.expression[-1] != '-'):
+            self.expression += '*'
+            self.multing = True
+            self.restore('*')
 
     def div(self):
-        self.expression += '/'
-        self.diving = True
-        self.restore('/')
+        """функция деления"""
+        if self.expression[-2:] != '//' and (self.expression[-1] != '*' and self.expression[-1] != '+' and
+                                             self.expression[-1] != '-'):
+            self.expression += '/'
+            self.diving = True
+            self.restore('/')
+
+    def ispoint(self):
+        """Проверка на то, можно ли ставить точку"""
+        expression = self.expression
+        res = ''
+        if '.' in self.expression:
+            while expression[-1] != '.':
+               res += expression[-1]
+               expression = expression[:-1]
+            if res.isdigit():
+                return False
+            else:
+                return True
+        else:
+            return True
 
     def point(self):
-        if '.' not in self.expression and self.expression:
+        """функция поставления точки"""
+        if self.expression != '' and self.ispoint() and self.expression[-1] != ')' and self.expression[-1] != '(':
             self.expression += '.'
 
     def fact(self):
+        """функция факториал"""
         self.expression = str(math.factorial(math.trunc(eval(self.expression))))
 
     def change(self):
-        self.expression = str(-eval(self.expression))
+        """функция изменения знака числа"""
+        if eval(self.expression) != 0 and self.expression != '':
+            self.expression = str(eval(self.expression) * (-1))
 
     def module(self):
+        """функция модуль"""
         self.expression = str(abs(eval(self.expression)))
 
-    def p(self):
-        if self.expression == '' or self.expression == '0':
-            self.expression = str(numpy.pi)
-        else:
-            self.expression = str(eval(str(self.expression)) * numpy.pi)
+    def back(self):
+        """функция очищения последнего знака"""
+        if len(self.expression) > 0:
+            self.expression = self.expression[:-1]
+            self.lineEdit_ex.setText(self.expression)
 
     def rd(self):
-        self.expression = str(eval(self.expression) ** (1 / 2))
+        """функция квадратного корня"""
+        if eval(self.expression) >= 0:
+            self.expression = str(eval(self.expression) ** (1 / 2))
 
     def rev(self):
-        self.expression = str(1 / eval(self.expression))
+        """нахождение обратного числа"""
+        if self.expression != '':
+            self.expression = '1/' + self.expression
 
     def sq(self):
-        self.expression = str(eval(self.expression) ** 2)
+        """возведение в квадрат"""
+        if eval(self.expression) != '':
+            self.expression = str(eval(self.expression) ** 2)
 
     def two(self):
-        self.expression = str(2 ** eval(self.expression))
+        """нахождение степени числа 2"""
+        if self.expression != '':
+            self.expression = '2**' + self.expression
 
     def ten(self):
-        self.expression = str(10 ** eval(self.expression))
+        """нахождение степени числа 10"""
+        if self.expression != '':
+            self.expression = str(10 ** eval(self.expression))
 
     def deg(self):
-        self.expression += '^'
-        x, y = self.expression.split('^')
+        """функция степени"""
+        self.expression += '**'
+        x, y = self.expression.split('**', maxsplit=True)
         if y:
             self.expression = str(eval(x) ** eval(y))
 
-    def mod(self):
-        self.expression += '≡'
-        x, y = self.expression.split('≡')
-        self.expression = str(math.fmod(math.trunc(eval(x)), math.trunc(eval(y))))
+    def round(self):
+        """функция округления числа до целого"""
+        self.expression = str(math.trunc(eval(self.expression)))
 
 
 if __name__ == '__main__':
